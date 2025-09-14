771e92cce6baeb10d63ae598169a50179b4ad5cd
your-new-username <your-new-email@example.com>
2025-09-14 17:32:52 +0530

    upadte json and npm install

--- routes/cart.routes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { updateCart } from "../controller/cart.controller.js";
-import authUser from "../middlewares/authUser.js";
-
-const router = express.Router();
-router.post("/update", authUser, updateCart);
-export default router;
